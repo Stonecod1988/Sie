b2b95710ce67003b181f2a870cebf97bcb8e1bc5
Bruce SIANI <sianibruce@gmail.com>
2021-01-11 14:27:28 +0100

    customisation du backoffice - ajout du template de base et filtre dans le listing

--- backend/Minepded/Sie/admin.py
+++ backend/Minepded/Sie/admin.py
@@ -3,10 +3,42 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+admin.site.site_header = 'Gestion du SIE'
 
-admin.site.register(Thematique)
-admin.site.register(Projet)
-admin.site.register(Programme)
-admin.site.register(Mesure)
-admin.site.register(CategorieMesure)
-admin.site.register(Indicateur)
+
+class ThematiqueAdmin(admin.ModelAdmin):
+    fields = ('intitule', 'programmes')
+    list_filter = ('intitule', 'programmes')
+
+
+class ProgrammeAdmin(admin.ModelAdmin):
+    fields = ('titre',)
+    list_filter = ('titre', 'thematiques')
+
+
+class ProjetAdmin(admin.ModelAdmin):
+    list_display = ('code_projet', 'intitule', 'description')
+    list_filter = ('code_projet', 'intitule', 'promoteur')
+
+
+class IndicateurAdmin(admin.ModelAdmin):
+    list_display = ('id', 'code_indicateur', 'mesure')
+    list_filter = ('code_indicateur', 'mesure')
+
+
+class MesureAdmin(admin.ModelAdmin):
+    list_display = ('id', 'type', 'descriptif', 'categorie_mesure')
+    list_filter = ('categorie_mesure',)
+
+
+class CategorieMesureAdmin(admin.ModelAdmin):
+    list_display = ('id', 'titre')
+    list_filter = ('titre',)
+
+
+admin.site.register(Thematique, ThematiqueAdmin)
+admin.site.register(Projet, ProjetAdmin)
+admin.site.register(Programme, ProgrammeAdmin)
+admin.site.register(Mesure, MesureAdmin)
+admin.site.register(CategorieMesure, CategorieMesureAdmin)
+admin.site.register(Indicateur, IndicateurAdmin)
